e5bf6aea628e78371da8790d88af6df01bdb0572
Arush Salil <me@aru.sh>
2024-02-28 19:55:42 +0530

    clean helm repo branch
    Signed-off-by: Arush Salil <me@aru.sh>

--- pkg/apis/secretgenerator/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the secretgenerator v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=secretgenerator.mittwald.de
-package v1alpha1
